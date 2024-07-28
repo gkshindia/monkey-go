53c84a2c6c27e76d473321431001791709c75f61
gourav-dronebase <gourav.singh@zeitview.com>
2024-07-28 17:35:02 +0530

    init lexer

---
+++ lexer/lexer.go
@@ -0,0 +1,60 @@
+package lexer
+
+import "monkey-go/token"
+
+// TODO: It is possible to support Unicode and Emojis - figure it out
+
+type Lexer struct {
+	input        string
+	position     int  // current position in input(points to current char)
+	nextPosition int  // current reading position in input (after current char)
+	ch           byte // current char under examination
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.nextPosition >= len(l.input) {
+		l.ch = 0 // ASCII code for NULL character and signifies eiter we haven't read anything yet or EOF
+	} else {
+		l.ch = l.input[l.nextPosition]
+	}
+	l.position = l.nextPosition
+	l.nextPosition += 1
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+	l.readChar()
+	return tok
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
